912c41601f27899419ed8d85282f037f233a9e36
rkrohne1991 <rafal.krohne@hotmail.co.uk>
2021-10-06 07:13:44 +0100

    Bug fix for use-http.js - wasn't getting any data

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -11,7 +11,7 @@ const useHttp = () => {
       const response = await fetch(requestConfig.url, {
         method: requestConfig.method ? requestConfig.method : "GET",
         headers: requestConfig.headers ? requestConfig.headers : {},
-        body: requestConfig.body ? JSON.stringify(requestConfig.body) : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
       });
 
       if (!response.ok) {
